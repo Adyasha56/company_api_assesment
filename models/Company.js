ef8426d5ae56a8f4dd81813d654ada290ff1f33b
Adyasha Nanda <adyashananda105@gmail.com>
2025-08-24 08:27:32 +0530

    initial commit

---
+++ models/Company.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const companySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  industry: { type: String, required: true },
+  location: { type: String, required: true },
+  size: { type: Number },
+  foundedYear: { type: Number },
+}, { timestamps: true });
+
+const Company = mongoose.model("Company", companySchema);
+
+export default Company;
